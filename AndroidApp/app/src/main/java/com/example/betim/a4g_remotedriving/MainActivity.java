b1ebd16b1985b84fbd7e16a5c54c73f127363504
betimr <betim@student.chalmers.se>
2017-02-06 13:26:58 +0100

    New upload of project

---
+++ AndroidApp/app/src/main/java/com/example/betim/a4g_remotedriving/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.betim.a4g_remotedriving;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void connect(View view) {
+        Intent intent = new Intent("com.example.betim.a4g_remotedriving.Client");
+
+        EditText ipField = (EditText) findViewById(R.id.ip_addr);
+        EditText portField = (EditText) findViewById(R.id.port_nr);
+
+        String ip = ipField.getText().toString();
+        String port = portField.getText().toString();
+
+        intent.putExtra("ipaddr",ip);
+        intent.putExtra("port",port);
+
+        startActivity(intent);
+    }
+}
